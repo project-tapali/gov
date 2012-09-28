3b87b44a1fe3b7d31a5cf05206cbdfa4d9dc0c01
Rahul Tapali <rahul.tapali@infibeam.net>
2012-09-28 20:27:46 +0530

    Creating rails app

---
+++ sn_gov/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SnGov::Application.config.secret_token = 'e51671b8e672131dfb027b5f2fdbbc4f143204e64c16f4f8c8239a6ff34afa836d9124f357aca0d55134b8bc24333a6d3afce45a9dfca39f31c13d2faf6e6ae0'
